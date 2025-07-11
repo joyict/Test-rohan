f35f26efad7e4927937ad7283cb698235c19278a
Jorrit <104207768+joyict@users.noreply.github.com>
2025-07-11 20:27:58 +0200

    Fix npm install and private key errors

--- src/components/config.js
+++ src/components/config.js
@@ -3,4 +3,4 @@ export const RPC_URL =
   "https://mainnet.infura.io/v3/4e9dee36fac640cdb852549e99360ec0";
 
 export const SECRET_KEY =
-"Your Private Key"
+"0x1234567890123456789012345678901234567890123456789012345678901234"
